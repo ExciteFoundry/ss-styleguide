2f08cf0082c5e1f787c28af3eb27ab16223762d8
pebiantara <pebiantara@gmail.com>
2024-12-31 20:12:14 +0700

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -76,10 +76,10 @@ const hbsHelpers = {
 };
 
 const hbsVars = {
-	imageUrl: './images',
-	jsUrl: './js',
-	cssUrl: './css',
-	criticalUrl: './critical-css',
+	imageUrl: '/images',
+	jsUrl: '/js',
+	cssUrl: '/css',
+	criticalUrl: '/critical-css',
 };
 
 const indexFile = function () {
